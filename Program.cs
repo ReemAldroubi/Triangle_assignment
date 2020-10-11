9f26449d8b5b1b7e648d550434e1272f68aa5e3a
ReemAldroubi <71336293+ReemAldroubi@users.noreply.github.com>
2020-10-11 16:11:07 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+
+                Console.WriteLine("1.Enter triangle dimensions");
+                Console.WriteLine("2.Exit");
+                String menu = Console.ReadLine();
+                int choice;
+                bool correctInput = Int32.TryParse(menu, out choice);
+                if (correctInput == true)
+                {
+
+                    switch (choice)
+                    {
+                        case 1:
+                            int a, b, c;
+                            Console.WriteLine("Enter three integers: ");
+                            Console.WriteLine("Enter the first integer: ");
+                            a = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter the second integer: ");
+                            b = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter the third integer: ");
+                            c = int.Parse(Console.ReadLine());
+                            string result = TriangleSolver.Analyze(a, b, c);
+                            Console.WriteLine(result);
+                            break;
+                        case 2:
+                            System.Environment.Exit(0);
+                            break;
+                        default:
+                            break;
+
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ TriangleSolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangle
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze(int a, int b, int c)
+        {
+            if (a + b <= c || a + c <= b || b + c <= a)
+                return "Not Triangle";
+            else
+            {
+                if (a == b && b == c)
+                {
+                    /* If all sides are equal */
+                    return "Equilateral triangle";
+                }
+                else if (a == b || a == c || b == c)
+                {
+                    /* If any two sides are equal */
+                    return "Isosceles triangle";
+                }
+                else
+                {
+                    /* If none sides are equal */
+                    return "Scalene triangle";
+                }
+            }
+        }
+    }
+}
